f5b5517958240ed50c80bdc36d1f5d665977d3ee
Peyton-Bell <bellpeyton248@gmail.com>
2026-05-26 12:15:11 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import SCREEN_HEIGHT
+from constants import SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    # initializing pygame
+    pygame.init()
+
+    # Startup message
+    print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # pygame GUI
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # infinite loop
+    while True:
+        # processing events in game
+        for event in pygame.event.get():
+            pass
+        # make screen black
+        screen.fill("black")
+
+        # update the display (must be called last)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()

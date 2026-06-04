a61ba074557c6009629a02c2d2b888417c9947ea
Peyton-Bell <bellpeyton248@gmail.com>
2026-06-04 09:48:18 -0500

    added support for W and S keys to move the player

--- player.py
+++ player.py
@@ -42,3 +42,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
